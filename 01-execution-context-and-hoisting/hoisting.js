2a2f3ca5573e3586fbdd900e588d89f66f25e656
Prabhakar <53590047+prabhakar-09@users.noreply.github.com>
2026-09-10 14:17:59 +0000

    exe context & hoisting

---
+++ 01-execution-context-and-hoisting/hoisting.js
@@ -0,0 +1,21 @@
+// Function vs Variable Hoisting
+console.log("1a:", myVar);
+console.log("1b:", myFunc);
+
+var myVar = 10;
+
+function myFunc() {
+  return "Hello from function!";
+}
+
+// Function Expression Hoisting
+try {
+  console.log("2:", expressFunc);
+  expressFunc();
+} catch (err) {
+  console.log("2 Error:", err.message);
+}
+
+var expressFunc = function () {
+  console.log("Running expression");
+};
